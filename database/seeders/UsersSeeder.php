82d788648c2e36a4e8ede2fc3832f688102c27ca
haseebm123 <haseeb@toobitech.com>
2023-12-06 21:55:42 +0500

    add

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -25,7 +25,7 @@ class UsersSeeder extends Seeder
 
         $demoUser2 = User::create([
             'name'              => $faker->name,
-            'email'             => 'admin@demo.com',
+            'email'             => 'admin@gmail.com',
             'password'          => Hash::make('demo'),
             'email_verified_at' => now(),
         ]);
--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -147,16 +147,16 @@
 
             <!--end:Menu item-->
             <!--begin:Menu item-->
-            <div class="menu-item pt-5">
+            {{-- <div class="menu-item pt-5">
                 <!--begin:Menu content-->
                 <div class="menu-content">
                     <span class="menu-heading fw-bold text-uppercase fs-7">Help</span>
                 </div>
                 <!--end:Menu content-->
-            </div>
+            </div> --}}
             <!--end:Menu item-->
             <!--begin:Menu item-->
-            <div class="menu-item">
+            {{-- <div class="menu-item">
                 <!--begin:Menu link-->
                 <a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities"
                     target="_blank">
@@ -164,20 +164,20 @@
                     <span class="menu-title">Components</span>
                 </a>
                 <!--end:Menu link-->
-            </div>
+            </div> --}}
             <!--end:Menu item-->
             <!--begin:Menu item-->
-            <div class="menu-item">
+            {{-- <div class="menu-item">
                 <!--begin:Menu link-->
                 <a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs" target="_blank">
                     <span class="menu-icon">{!! getIcon('abstract-26', 'fs-2') !!}</span>
                     <span class="menu-title">Documentation</span>
                 </a>
                 <!--end:Menu link-->
-            </div>
+            </div> --}}
             <!--end:Menu item-->
             <!--begin:Menu item-->
-            <div class="menu-item">
+            {{-- <div class="menu-item">
                 <!--begin:Menu link-->
                 <a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs/changelog"
                     target="_blank">
@@ -185,7 +185,7 @@
                     <span class="menu-title">Changelog v8.2.0</span>
                 </a>
                 <!--end:Menu link-->
-            </div>
+            </div> --}}
             <!--end:Menu item-->
         </div>
         <!--end::Menu-->
